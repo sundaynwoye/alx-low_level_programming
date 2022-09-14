bc40d56390b58d21c6c60142302ddf0b05e2542c
sundaynwoye <sundaynwoye23@gmail.com>
2022-09-13 22:03:44 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,33 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
-		for (n = n; n <= 97; n++)
-			printf("%d, ", n);
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
 	}
 	else
 	{
-		for (n = n; n > 98; n--)
-			printf("%d, ", n);
+		printf("98");
 	}
-	printf("98\n");
+	printf("\n");
 }
